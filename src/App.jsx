1d375d0d3e27ce736986954640341ff2b18d5e5a
reddevill007 <shivampandey2900@gmail.com>
2022-06-09 08:14:59 +0530

    basic npm install

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+      <small>Hello this is me</small>
+    </>
+  )
+}
+
+export default App;
