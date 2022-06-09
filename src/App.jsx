c3e8c2c62b56c8ee20b97cf50d6e8d3c06593624
reddevill007 <shivampandey2900@gmail.com>
2022-06-09 08:46:41 +0530

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Board from "./components/Board";
+import './styles/root.scss';
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <h1>Tic Tac Toe</h1>
       <Board />
     </div>
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 const Square = ({ value }) => {
     return (
-        <button type='button'>{value}</button>
+        <button type='button' className='square'>{value}</button>
     )
 }
 
