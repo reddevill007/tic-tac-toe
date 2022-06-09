21ec686e64583882cc217783f8243daa39a5082d
reddevill007 <shivampandey2900@gmail.com>
2022-06-09 09:12:47 +0530

    added board state

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const handleSquareClick = (position) => {
+    if (board[position]) {
+      return;
+    }
+
+    setBoard((prev) => {
+      return prev.map((square, pos) => {
+
+        if (pos === position) {
+          return isXNext ? 'X' : '0';
+        }
+
+        return square;
+      })
+    })
+
+    setIsXNext((prev) => !prev)
+  };
+
+  const renderSquare = (pos) => {
+    return (
+      <Square
+        value={board[pos]}
+        onClick={() => handleSquareClick(pos)}
+      />
+    )
+  }
   return (
     <div className='board'>
       <div className='board-row'>
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className='board-row'>
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className='board-row'>
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const Square = ({ value }) => {
+const Square = ({ value, onClick }) => {
     return (
-        <button type='button' className='square'>{value}</button>
+        <button
+            type='button'
+            className='square'
+            onClick={onClick}
+        >
+            {value}
+        </button>
     )
 }
 
